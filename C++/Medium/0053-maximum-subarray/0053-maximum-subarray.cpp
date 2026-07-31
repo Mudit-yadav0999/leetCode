9874e060417900f1ff9aa85cceb07919ae9f6bae
Mudit yadav <muditsing704@gmail.com>
2026-08-01 01:12:56 +0530

    0053-maximum-subarray Medium .cpp

--- C++/Medium/0053-maximum-subarray/0053-maximum-subarray.cpp
+++ C++/Medium/0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -3,8 +3,9 @@ public:
     int maxSubArray(vector<int>& nums) {
         int sum=0;
         int maxsum=INT_MIN;
-       for(int i=0;i<nums.size();i++){
-        sum+=nums[i];
+       //for(int i=0;i<nums.size();i++)
+       for(int value:nums){
+        sum+=value;
         maxsum=max(sum,maxsum);
         if(sum<0){
             sum=0;
