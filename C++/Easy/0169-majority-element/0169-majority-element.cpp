9be5f136c88bd115ae594a4355d99b5e7349b4d5
Mudit yadav <muditsing704@gmail.com>
2026-08-01 02:26:34 +0530

    0169-majority-element Easy .cpp

--- C++/Easy/0169-majority-element/0169-majority-element.cpp
+++ C++/Easy/0169-majority-element/0169-majority-element.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
+        //method 1 x
         // int n=nums.size();
         // for(int i:nums){
         //     int freq=0;
@@ -13,22 +14,39 @@ public:
         //     return i;
         // }
         // return -1;
-        int n=nums.size();
-        sort(nums.begin(),nums.end());
-        int freq=1;
-        int ans=nums[0];
-        for(int i=1;i<n;i++){
-            if(nums[i]==nums[i-1]){
+
+        //Method2
+        // int n=nums.size();
+        // sort(nums.begin(),nums.end());
+        // int freq=1;
+        // int ans=nums[0];
+        // for(int i=1;i<n;i++){
+        //     if(nums[i]==nums[i-1]){
+        //         freq++;
+        //     }
+        //     else{
+        //         freq=1;
+        //         ans=nums[i];
+        //     }
+        //     if(freq>n/2){
+        //         return ans;
+        //     }
+        //}
+        
+
+        //method 3;
+        int ans=0;
+        int freq=0;
+        for(int i=0;i<nums.size();i++){
+            if(freq==0){
+                ans=nums[i];
+            }
+            if(ans==nums[i]){
                 freq++;
             }
             else{
-                freq=1;
-                ans=nums[i];
+                freq--;
             }
-            if(freq>n/2){
-                return ans;
-            }
-        
         }
         return ans;
     }
