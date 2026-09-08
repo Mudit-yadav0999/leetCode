01f7314f82cd292e86781bd33b1d809ed6f2b951
Mudit yadav <muditsing704@gmail.com>
2026-09-08 22:02:01 +0530

    3870-count-commas-in-range Easy .cpp

--- C++/Easy/3870-count-commas-in-range/3870-count-commas-in-range.cpp
+++ C++/Easy/3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int countCommas(int n) {
-        if(n<=999)return 0;
-        int comma=0;
-        for(int i=1000;i<=n;i++){
-            comma++;
-        }
-        return comma;
+       if(n>=1000){
+        return (n-999);
+       }
+       else {
+        return 0;
+       }
     }
 };
